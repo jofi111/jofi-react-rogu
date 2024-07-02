72a0fe263bcbc649dd7fa2d60b0baa68bb83ad4a
Jofi <jofi1@seznam.cz>
2024-07-02 23:31:26 +0200

    uprava fce a hooks pro aktualizaci canvasu, context

--- src/JofiReactRogue.js
+++ src/JofiReactRogue.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-const RRogue = ({ width, height, tilesize }) => (
-  <canvas
-    width={width * tilesize}
-    height={height * tilesize}
-    style={{ border: "1px solid black" }}
-  ></canvas>
-);
+const RRogue = ({ width, height, tilesize }) => {
+  const canvasRef = React.useRef();
+  useEffect(() => {
+    console.log("Draw to canvas");
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(12, 12, 16, 16);
+  });
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width * tilesize}
+      height={height * tilesize}
+      style={{ border: "1px solid black" }}
+    ></canvas>
+  );
+};
 
 export default RRogue;
