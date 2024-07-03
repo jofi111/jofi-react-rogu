4449d2846efa7cbb5e15aa43d70fe430802cdb00
Jofi <jofi1@seznam.cz>
2024-07-03 22:38:24 +0200

    oprava a zprovozneni s pomoci chatGPT

--- src/JofiReactRogue.js
+++ src/JofiReactRogue.js
@@ -4,24 +4,25 @@ import InputManager from "./InputManager.js";
 const RRogue = ({ width, height, tilesize }) => {
   const canvasRef = useRef();
   const [player, setPlayer] = useState({ x: 64, y: 128 });
-  let InputManager = new InputManager();
+  const inputManager = useRef(new InputManager()).current;
+
   const handleInput = (action, data) => {
     console.log(`handle input: ${action}:${JSON.stringify(data)}`);
-    let newPlayer = { ...player };
-    newPlayer.x += data.x * tilesize;
-    newPlayer.y += data.y * tilesize;
-    setPlayer(newPlayer);
+    setPlayer((prevPlayer) => ({
+      x: prevPlayer.x + data.x * tilesize,
+      y: prevPlayer.y + data.y * tilesize,
+    }));
   };
 
   useEffect(() => {
     console.log("Bind input");
-    InputManager.bindKeys();
-    InputManager.subscribe(handleInput);
+    inputManager.bindKeys();
+    inputManager.subscribe(handleInput);
     return () => {
-      InputManager.unbindKeys();
-      InputManager.subscribe(handleInput);
+      inputManager.unbindKeys();
+      inputManager.unsubscribe(handleInput);
     };
-  });
+  }, [inputManager]);
 
   useEffect(() => {
     console.log("Draw to canvas");
