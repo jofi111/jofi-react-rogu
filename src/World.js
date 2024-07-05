6adeeab698c15451307408c243bcfe85ec95b6cc
Jofi <jofi1@seznam.cz>
2024-07-06 00:05:33 +0200

    donastaveni vykreslovani mapy pomoci rot-js, aby cesty byly propojene a okraj pole byl lemovan zdi

--- src/World.js
+++ src/World.js
@@ -9,14 +9,21 @@ class World {
     for (let x = 0; x < this.width; x++) {
       this.worldmap[x] = new Array(this.height);
     }
-    this.createRandomMap();
+    this.createCellularMap();
   }
-  createRandomMap() {
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        this.worldmap[x][y] = Math.round(Math.random());
+  createCellularMap() {
+    var map = new Map.Cellular(this.width, this.height, { connected: true });
+    map.randomize(0.5);
+    //callback slouzi k vykresleni zdi po okraji plochy
+    var userCallback = (x, y, value) => {
+      if (x === 0 || y === 0 || x === this.width - 1 || y === this.height - 1) {
+        this.worldmap[x][y] = 1;
+        return;
       }
-    }
+      this.worldmap[x][y] = value === 0 ? 1 : 0;
+    };
+    map.create(userCallback);
+    map.create(userCallback, 1);
   }
 
   draw(context) {
