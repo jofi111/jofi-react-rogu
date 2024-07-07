17b25ee9b1baee81b7a71d14c8faa472727b5edf
Jofi <jofi1@seznam.cz>
2024-07-07 21:35:28 +0200

    nastaveni p[ohybu hrace ve svete

--- src/JofiReactRogue.js
+++ src/JofiReactRogue.js
@@ -5,18 +5,31 @@ import World from "./World.js";
 
 const RRogue = ({ width, height, tilesize }) => {
   const canvasRef = useRef();
-  const [player, setPlayer] = useState(new Player(1, 2, tilesize));
+  //const [player, setPlayer] = useState(new Player(1, 2, tilesize));
   const [world, setWorld] = useState(new World(width, height, tilesize));
   const inputManager = useRef(new InputManager()).current;
 
   const handleInput = (action, data) => {
     console.log(`handle input: ${action}:${JSON.stringify(data)}`);
-    setPlayer((prevPlayer) => {
-      const newPlayer = new Player(prevPlayer.x, prevPlayer.y, prevPlayer.size);
-      newPlayer.move(data.x, data.y);
-      return newPlayer;
-    });
+    let newWorld = new World();
+    Object.assign(newWorld, world);
+    newWorld.movePlayer(data.x, data.y);
+    setWorld(newWorld);
+    // setPlayer((prevPlayer) => {
+    //   const newPlayer = new Player(prevPlayer.x, prevPlayer.y, prevPlayer.size);
+    //   newPlayer.move(data.x, data.y);
+    //   return newPlayer;
+    // });
   };
+
+  useEffect(() => {
+    console.log("Create Map!");
+    let newWorld = new World();
+    Object.assign(newWorld, world);
+    newWorld.createCellularMap();
+    newWorld.moveToSpace(world.player);
+    setWorld(newWorld);
+  }, []); //prazdny parametr [] brani opakovanemu renderovani mapy pri kazdem pohybu hrace
 
   useEffect(() => {
     console.log("Bind input");
@@ -33,8 +46,9 @@ const RRogue = ({ width, height, tilesize }) => {
     const ctx = canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, width * tilesize, height * tilesize);
     world.draw(ctx);
-    player.draw(ctx);
-  });
+    //player.draw(ctx);
+  }, [world, width, height, tilesize]);
+
   return (
     <canvas
       ref={canvasRef}
